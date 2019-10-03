318e0c692f313aa93f9bba8d7e4e79727d02cf39
Shishir Sharma <260073@via.dk>
2019-10-03 15:57:14 +0200

    added asignment7.

---
+++ Assignment7/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Assignment7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private Random m_Random = new Random();
+
+        public void HeavyWork()
+        {
+            //generate value betweeen 0 to 10
+            double secondsToSleep = m_Random.NextDouble() * 10;
+
+            //simulate important work
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(secondsToSleep));
+        }
+
+        public Task HeavyWorkAsyn()
+        {
+            //run HeavyWork asynchronously
+            return Task.Run(() => HeavyWork());
+        }
+    }
+}
