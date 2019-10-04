fb693192f4d6607da80117a4e05b617c72cd30c3
Shishir Sharma <260073@via.dk>
2019-10-04 12:03:20 +0200

    assignment 7 measuring of parallel threads, still last question left to do.

--- Assignment7/MainWindow.xaml.cs
+++ Assignment7/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ namespace Assignment7
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
@@ -30,5 +31,44 @@ namespace Assignment7
             //run HeavyWork asynchronously
             return Task.Run(() => HeavyWork());
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            label.Content += "Heavy Work has started at " + DateTime.Now.ToLongTimeString() + "! \n";
+
+            Parallel.Invoke(
+                            () =>
+                            {
+                                Console.WriteLine("TASK 1 STARTED!");
+                                HeavyWork();
+                                Console.WriteLine("TASK 1 DONE!");
+                            },
+                            () =>
+                            {
+                                Console.WriteLine("TASK 2 STARTED!");
+                                HeavyWork();
+                                Console.WriteLine("TASK 2 DONE!");
+                            },
+                            () =>
+                            {
+                                Console.WriteLine("TASK 3 STARTED!");
+                                HeavyWork();
+                                Console.WriteLine("TASK 3 DONE!");
+                            }
+                         );
+            label.Content += "All Heavy Work has ended " + DateTime.Now.ToLongTimeString() + "!\n";
+
+            //await HeavyWorkAsyn();
+
+
+            label.Content += "\n";
+
+        }
+
+        #region PARALLEL EXECUTION
+            //All the task start at the same time as they are been exected at parallelly. 
+            //They end at different time because their is a random number of sleep for the heavywork().
+            //Output can been seen in the output console.
+        #endregion
     }
 }
